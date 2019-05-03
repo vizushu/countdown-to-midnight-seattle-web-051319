05da893bb723f5668040bc73b65241a1c78ee113
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 19:12:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@
 
 def countdown(number)
   number = 10
-  while number < 
-    puts "SECOND(S)!"
+  while number > 0
+    puts "#{number} SECOND(S)!"
   number -= 1
   return "HAPPY NEW YEAR!"
   end
