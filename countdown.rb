347357bd15d0490d17968115eb2015d615262140
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 19:20:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,14 +4,6 @@ def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
   number -= 1
-  
-  endreturn "HAPPY NEW YEAR!"
-end
-
-def countdown(int)
-while int > 0
-  puts "#{int} SECOND(S)!"
- int -= 1
-end
-return "HAPPY NEW YEAR!"
+  end
+  return "HAPPY NEW YEAR!"
 end
