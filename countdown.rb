f782458509123a76e5d0e716d27e8ffd918e6ff8
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 19:09:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,10 @@
 
 
 def countdown(number)
-  number = 0
+  number = 10
   while number < 
     puts "#{number} SECOND(S)!"
-  number -=
-end
+  number -= 1
+  end
   return "HAPPY NEW YEAR!"
-end
-
-counter = 0
-while counter < 20
-  puts "The current number is less than 20."
-  counter += 1
 end
