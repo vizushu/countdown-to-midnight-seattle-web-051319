f646106d5250f58a751bd5ab1f5c1a17e4332fcf
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 19:22:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+  sleep 5
+    puts "#{number} SECOND(S)!"
+  number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
