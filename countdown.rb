709dc1286650c974e3e3bacbc908c0d3fc692d47
beneficial-abstraction-6109 <me@renierdelacruz.com>
2019-05-03 19:07:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,14 @@
 
 
 def countdown(argument)
-  while 
+  argument = 0
+  while argument < 
   
+end
+
+
+counter = 0
+while counter < 20
+  puts "The current number is less than 20."
+  counter += 1
 end
